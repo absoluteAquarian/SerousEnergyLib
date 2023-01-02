f3d39a76e8b9cff7b2d4452eddb768918f66fe73
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2023-01-02 17:33:41 -0600

    added missing overrides in PowerNetwork

--- src/Systems/Networks/PowerNetwork.cs
+++ src/Systems/Networks/PowerNetwork.cs
@@ -1,10 +1,32 @@
 ﻿using SerousEnergyLib.API.Energy;
 using SerousEnergyLib.TileData;
+using SerousEnergyLib.Tiles;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
 
 namespace SerousEnergyLib.Systems.Networks {
 	public sealed class PowerNetwork : NetworkInstance {
 		public FluxStorage Storage = new FluxStorage(TerraFlux.Zero);
 
 		internal PowerNetwork() : base(NetworkType.Power) { }
+
+		public override void OnEntryAdded(Point16 location) {
+			Tile tile = Main.tile[location.X, location.Y];
+
+			if (TileLoader.GetTile(tile.TileType) is IPowerTransportTile transport)
+				Storage.MaxCapacity += transport.MaxCapacity;
+		}
+
+		public override void OnEntryRemoved(Point16 location) {
+			Tile tile = Main.tile[location.X, location.Y];
+
+			if (TileLoader.GetTile(tile.TileType) is IPowerTransportTile transport)
+				Storage.MaxCapacity -= transport.MaxCapacity;
+
+			// Clamp the storage
+			if (Storage.CurrentCapacity > Storage.MaxCapacity)
+				Storage.CurrentCapacity = Storage.MaxCapacity;
+		}
 	}
 }
