36cf81c499774fc1a484a16661d2b3b875832ff2
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2023-01-06 14:59:36 -0600

    more upgrade functions and interfaces

---
+++ src/API/Machines/IFluidOutputGeneratorMachine.cs
@@ -0,0 +1,23 @@
+﻿using SerousEnergyLib.API.Upgrades;
+using System.Linq;
+using Terraria.ModLoader;
+
+namespace SerousEnergyLib.API.Machines {
+	/// <summary>
+	/// An interface representing a machine that can create fluid outputs<br/>
+	/// This interface is used with <see cref="BaseUpgrade"/>
+	/// </summary>
+	public interface IFluidOutputGeneratorMachine : IFluidMachine, IMachine {
+		/// <summary>
+		/// Modifies <paramref name="originalProduct"/> based on the upgrades contained in this machine
+		/// </summary>
+		/// <param name="originalProduct">The original product quantity in Liters</param>
+		/// <param name="slot">Which fluid storage slot in <see cref="IFluidMachine.FluidStorage"/> the output will end up in</param>
+		/// <returns>The modified product quantity</returns>
+		public double CalculateFluidProduct(double originalProduct, int slot)
+			=> CalculateFromUpgrades(StatModifier.Default,
+				Upgrades.Where(u => CanUpgradeApplyTo(u, slot)),
+				static (u, v) => u.GetFluidOutputGeneratorProductMultiplier().CombineWith(v))
+				.ApplyTo(originalProduct);
+	}
+}
---
+++ src/API/Machines/IItemOutputGeneratorMachine.cs
@@ -0,0 +1,16 @@
+﻿using SerousEnergyLib.API.Upgrades;
+
+namespace SerousEnergyLib.API.Machines {
+	/// <summary>
+	/// An interface representing a machine that can create item outputs<br/>
+	/// This interface is used with <see cref="BaseUpgrade"/>
+	/// </summary>
+	public interface IItemOutputGeneratorMachine : IInventoryMachine, IMachine {
+		/// <summary>
+		/// Modifies <paramref name="originalStack"/> based on the upgrades contained in this machine
+		/// </summary>
+		/// <param name="originalStack">The original stack</param>
+		/// <returns>The modified item stack</returns>
+		public int CalculateItemStack(int originalStack) => CalculateFromUpgrades(originalStack, static (u, v) => v + u.GetItemOutputGeneratorExtraStack(v));
+	}
+}
