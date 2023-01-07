3234c1fc4f2be91e671c9dba904c6ac9051541c4
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2023-01-06 18:44:41 -0600

    added structs for streamlined machine effect drawing

---
+++ src/API/CrossMod/MachineSpriteEffectData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace SerousEnergyLib.API.CrossMod {
+	#pragma warning disable CS1591
+	public struct MachineSpriteEffectData {
+		public Asset<Texture2D> asset;
+		public Vector2 offset;
+		public Rectangle? frame;
+		public Color color;
+		public float rotation;
+		public Vector2 origin;
+		public Vector2 scale;
+		public SpriteEffects effects;
+
+		public bool affectedByLight;
+
+		public MachineSpriteEffectData(MachineSpriteEffectInformation info) {
+			asset = ModContent.Request<Texture2D>(info.asset);
+			offset = info.offset;
+			frame = info.frame;
+			color = Color.White;
+			rotation = 0;
+			origin = Vector2.Zero;
+			scale = Vector2.One;
+			effects = SpriteEffects.None;
+			affectedByLight = info.affectedByLight;
+		}
+
+		/// <summary>
+		/// Uses the data in this instance to draw the effect sprite
+		/// </summary>
+		/// <param name="spriteBatch">The sprite batch</param>
+		/// <param name="entityLocation">The location of the source <see cref="ModTileEntity"/> that this effect is drawn for</param>
+		public void Draw(SpriteBatch spriteBatch, Point16 entityLocation) {
+			Vector2 worldPosition = entityLocation.ToWorldCoordinates(0, 0) + offset;
+			Vector2 drawPosition = worldPosition - Main.screenPosition + TileFunctions.GetLightingDrawOffset();
+
+			Point tilePosition = worldPosition.ToTileCoordinates();
+
+			Color drawColor = affectedByLight ? Lighting.GetColor(tilePosition, color) : color;
+
+			spriteBatch.Draw(asset.Value, drawPosition, frame, drawColor, rotation, origin, scale, effects, 0);
+		}
+	}
+}
---
+++ src/API/CrossMod/MachineSpriteEffectInformation.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SerousEnergyLib.API.CrossMod {
+	/// <summary>
+	/// A structure representing data used to draw an overlay behind or in front of a machine
+	/// </summary>
+	public readonly struct MachineSpriteEffectInformation {
+		/// <summary>
+		/// The path to the effect's sprite
+		/// </summary>
+		public readonly string asset;
+
+		/// <summary>
+		/// Whether the effect draws behind the machine (<see langword="true"/>) or in front of it (<see langword="false"/>)
+		/// </summary>
+		public readonly bool drawBehind;
+
+		/// <summary>
+		/// The offset from the top-left corner of the machine tile to draw the effect at<br/>
+		/// <b>NOTE:</b> This field is in world coordinates, not tile coordinates
+		/// </summary>
+		public readonly Vector2 offset;
+
+		/// <summary>
+		/// The source rectangle within this effect's sprite to draw
+		/// </summary>
+		public readonly Rectangle? frame;
+
+		/// <summary>
+		/// Whether this effect's draw color is affected by the lighting in the world where it's drawn
+		/// </summary>
+		public readonly bool affectedByLight;
+
+		/// <summary>
+		/// Creates a new <see cref="MachineSpriteEffectInformation"/> instance
+		/// </summary>
+		/// <param name="asset">The path to the effect's sprite</param>
+		/// <param name="drawBehind">Whether the effect draws behind the machine (<see langword="true"/>) or in front of it (<see langword="false"/>)</param>
+		/// <param name="offset">
+		/// The offset from the top-left corner of the machine tile to draw the effect at<br/>
+		/// <b>NOTE:</b> This parameter is in world coordinates, not tile coordinates
+		/// </param>
+		/// <param name="frame">The source rectangle within this effect's sprite to draw</param>
+		/// <param name="affectedByLight">Whether this effect's draw color is affected by the lighting in the world where it's drawn</param>
+		public MachineSpriteEffectInformation(string asset, bool drawBehind, Vector2 offset, Rectangle? frame, bool affectedByLight) {
+			this.asset = asset;
+			this.drawBehind = drawBehind;
+			this.offset = offset;
+			this.frame = frame;
+			this.affectedByLight = affectedByLight;
+		}
+
+		/// <summary>
+		/// Creates a new <see cref="MachineSpriteEffectData"/> instance based on this effect information
+		/// </summary>
+		public MachineSpriteEffectData GetDrawInformation() => new MachineSpriteEffectData(this);
+	}
+}
