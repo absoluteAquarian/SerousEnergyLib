221e5dc4d0bdd73f277a9bc90f9e37c2c9abc47f
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2023-01-09 15:10:27 -0600

    added operators and comparison methods to Ticks

--- src/API/Ticks.cs
+++ src/API/Ticks.cs
@@ -1,8 +1,10 @@
-﻿namespace SerousEnergyLib.API {
+﻿using System;
+
+namespace SerousEnergyLib.API {
 	/// <summary>
 	/// A structure representing a duration of time, measured in game ticks
 	/// </summary>
-	public readonly struct Ticks {
+	public readonly struct Ticks : IEquatable<Ticks>, IComparable<Ticks> {
 		#pragma warning disable CS1591
 		public readonly int ticks;
 
@@ -17,5 +19,41 @@
 		public static Ticks FromSeconds(float seconds) => new((int)(seconds * 60));
 
 		public static Ticks FromSeconds(double seconds) => new((int)(seconds * 60));
+
+		public override bool Equals(object obj) => obj is Ticks ticks && Equals(ticks);
+
+		public bool Equals(Ticks other) => ticks == other.ticks;
+
+		public override int GetHashCode() => ticks;
+
+		public int CompareTo(Ticks other) => ticks.CompareTo(other.ticks);
+
+		public static bool operator ==(Ticks left, Ticks right) => left.Equals(right);
+
+		public static bool operator !=(Ticks left, Ticks right) => !(left == right);
+
+		public static bool operator <(Ticks left, Ticks right) => left.ticks < right.ticks;
+
+		public static bool operator <=(Ticks left, Ticks right) => left.ticks <= right.ticks;
+
+		public static bool operator >(Ticks left, Ticks right) => left.ticks > right.ticks;
+
+		public static bool operator >=(Ticks left, Ticks right) => left.ticks >= right.ticks;
+
+		public static bool operator <(Ticks left, int right) => left.ticks < right;
+
+		public static bool operator <=(Ticks left, int right) => left.ticks <= right;
+
+		public static bool operator >(Ticks left, int right) => left.ticks > right;
+
+		public static bool operator >=(Ticks left, int right) => left.ticks >= right;
+
+		public static bool operator <(int left, Ticks right) => left < right.ticks;
+
+		public static bool operator <=(int left, Ticks right) => left <= right.ticks;
+
+		public static bool operator >(int left, Ticks right) => left > right.ticks;
+
+		public static bool operator >=(int left, Ticks right) => left >= right.ticks;
 	}
 }
