180252c77f5c1e490e337daa7aa3cbd4d656816c
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2023-01-16 15:01:47 -0600

    de-nested `NetworkSearchResult`

--- src/API/Machines/IMachine.cs
+++ src/API/Machines/IMachine.cs
@@ -323,32 +323,6 @@ namespace SerousEnergyLib.API.Machines {
 		}
 
 		/// <summary>
-		/// A struct representing a search result for an adjacent network
-		/// </summary>
-		public readonly struct NetworkSearchResult {
-			/// <summary>
-			/// The adjacent network instance
-			/// </summary>
-			public readonly NetworkInstance network;
-
-			/// <summary>
-			/// The tile in the network that was adjacent to <see cref="machineTileAdjacentToNetwork"/>
-			/// </summary>
-			public readonly Point16 tileInNetwork;
-
-			/// <summary>
-			/// The tile within this machine that is adjacent to the network
-			/// </summary>
-			public readonly Point16 machineTileAdjacentToNetwork;
-
-			internal NetworkSearchResult(NetworkInstance instance, Point16 location, Point16 adjacentLocation) {
-				network = instance;
-				tileInNetwork = location;
-				machineTileAdjacentToNetwork = adjacentLocation;
-			}
-		}
-
-		/// <summary>
 		/// Returns an enumeration of the adjacent networks to <paramref name="machine"/> which satisfy the filter, <paramref name="type"/>
 		/// </summary>
 		/// <param name="machine">The machine to process</param>
---
+++ src/API/NetworkSearchResult.cs
@@ -0,0 +1,30 @@
+﻿using SerousEnergyLib.Systems;
+using Terraria.DataStructures;
+
+namespace SerousEnergyLib.API {
+	/// <summary>
+	/// A struct representing a search result for an adjacent network
+	/// </summary>
+	public readonly struct NetworkSearchResult {
+		/// <summary>
+		/// The adjacent network instance
+		/// </summary>
+		public readonly NetworkInstance network;
+
+		/// <summary>
+		/// The tile in the network that was adjacent to <see cref="machineTileAdjacentToNetwork"/>
+		/// </summary>
+		public readonly Point16 tileInNetwork;
+
+		/// <summary>
+		/// The tile within this machine that is adjacent to the network
+		/// </summary>
+		public readonly Point16 machineTileAdjacentToNetwork;
+
+		internal NetworkSearchResult(NetworkInstance instance, Point16 location, Point16 adjacentLocation) {
+			network = instance;
+			tileInNetwork = location;
+			machineTileAdjacentToNetwork = adjacentLocation;
+		}
+	}
+}
